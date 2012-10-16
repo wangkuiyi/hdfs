a4ac441ea094584fd8113501d974dc5228cbd1db
Yi Wang <yi.wang.2005@gmail.com>
2012-10-16 16:20:00 +0800

    Add hdfs.go and unit test

---
+++ hdfs.go
@@ -0,0 +1,86 @@
+package hdfs
+
+import (
+	"errors"
+	zyxar "github.com/zyxar/hdfs"
+	"io"
+	"log"
+	"runtime"
+)
+
+var (
+	filesystem *zyxar.Fs
+	ErrInvalid = errors.New("Invalid argument")
+)
+
+func init() {
+	Init("", 0, "") // Connect to local filesystem.
+}
+
+// Init connects to an HDFS service.  If host is an empty string, it
+// connects to the local filesystem.  Indeed, the package is
+// initialized to connect to the local filesystem.  If you want to
+// connect to a HDFS, recall Init (in main, for example).
+func Init(host string, port uint16, user string) error {
+	var err error
+	filesystem, err = zyxar.ConnectAsUser(host, port, user)
+	if err != nil {
+		log.Fatal("Cannot connect to local filesystem")
+	}
+	return err
+}
+
+type File struct {
+	file *zyxar.File
+	name string
+}
+
+func Open(name string) (*File, error) {
+	return openFile(name, zyxar.O_RDONLY)
+}
+
+func Create(name string) (file *File, err error) {
+	return openFile(name, zyxar.O_WRONLY|zyxar.O_CREATE)
+}
+
+func openFile(name string, flags int) (*File, error) {
+	hdfsFile, e := filesystem.OpenFile(name, flags, 0, 0, 0)
+	if hdfsFile == nil || e != nil {
+		return nil, e
+	}
+
+	runtime.SetFinalizer(hdfsFile, func(file *zyxar.File) {
+		filesystem.CloseFile(file)
+	})
+	f := &File{hdfsFile, name}
+	return f, e
+}
+
+func (f *File) Read(b []byte) (n int, err error) {
+	if f == nil {
+		return 0, ErrInvalid
+	}
+	u, e := filesystem.Read(f.file, b, len(b))
+	if u == 0 && len(b) > 0 && e == nil {
+		return 0, io.EOF
+	}
+	return int(u), e
+}
+
+func (f *File) Write(b []byte) (n int, err error) {
+	if f == nil {
+		return 0, ErrInvalid
+	}
+	u, e := filesystem.Write(f.file, b, len(b))
+	return int(u), e
+}
+
+func (f *File) Close() error {
+	if f.file == nil {
+		return ErrInvalid
+	}
+	e := filesystem.CloseFile(f.file)
+	runtime.SetFinalizer(f.file, nil) // Finalizer is no longer needed.
+	f.file = nil                      // Prevents from closing it again.
+	return e
+}
---
+++ hdfs_test.go
@@ -0,0 +1,51 @@
+package hdfs
+
+import (
+	"bufio"
+	"testing"
+)
+
+func testCreateAndRead(filename, content string, t *testing.T) {
+	f, e := Create(filename)
+	if e != nil {
+		t.Errorf("Cannot create file %s : %v", filename, e)
+	}
+	w := bufio.NewWriter(f)
+	w.WriteString(content)
+	w.Flush()
+	f.Close()
+
+	f, e = Open(filename)
+	if e != nil {
+		t.Errorf("Cannot open file %s : %v", filename, e)
+	}
+	r := bufio.NewReader(f)
+	s, e := r.ReadString('\n')
+	if e != nil {
+		t.Errorf("Failed reading string")
+	}
+
+	if s != content {
+		t.Errorf("Expecting %s, got %s", content, s)
+	}
+	f.Close()
+}
+
+func TestLocalFS(t *testing.T) {
+	const (
+		kFilename = "/tmp/a.txt"
+		kContent  = "Local FS, Go!\n"
+	)
+	testCreateAndRead(kFilename, kContent, t)
+}
+
+func TestHDFS(t *testing.T) {
+	const (
+		kFilename = "/tmp/a.txt"
+		kContent  = "Hadoop, Go!\n"
+	)
+	// NOTE: here we assumed that HDFS had been deployed on
+	// localhost:9000.  If this is not your case, please change.
+	Init("localhost", 9000, "")
+	testCreateAndRead(kFilename, kContent, t)
+}
